cb34d7fd9e3beae93f595bf1cf80466acd214e6c
wekesajabz <wekesajabez6550@gmail.com>
2024-08-05 16:53:30 +0300

    added the links for the navbar

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,16 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <div className="project-list">
+      {projects.map((project) => (
+        <ProjectItem
+          key={project.id}
+          name={project.name}
+          description={project.description}
+          technologies={project.technologies}
+        />
+      ))}
     </div>
   );
 }
